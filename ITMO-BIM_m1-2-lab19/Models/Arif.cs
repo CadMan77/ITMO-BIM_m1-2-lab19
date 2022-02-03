ffb0f188f45f0e06a77f80f45776b956e348f831
cadman77 <cadman77@yandex.ru>
2022-02-04 00:41:03 +0300

    Add project files.

---
+++ ITMO-BIM_m1-2-lab19/Models/Arif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO_BIM_m1_2_lab19.Models
+{
+    static class Arif
+    {
+        public static double LenCalc(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ ITMO-BIM_m1-2-lab19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using ITMO_BIM_m1_2_lab19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ITMO_BIM_m1_2_lab19.ViewModels
+{
+    class MainWindowViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //void OnPropertyChanged(string PropertyName = null)
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double circleLength;
+        public double CircleLength
+        {
+            get => circleLength;
+            set
+            {
+                circleLength = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand LenCalcCommand { get; }
+        private void OnLenCalcCommandExecute(object p)
+        {
+            CircleLength = Arif.LenCalc(Radius);
+        }
+
+        private bool CanLenCalcCommandExecuted(object p)
+        {
+            if (Radius > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            LenCalcCommand = new RelayCommand(OnLenCalcCommandExecute, CanLenCalcCommandExecuted);
+        }
+    }
+}
